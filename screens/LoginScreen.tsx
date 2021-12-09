546fef88657c338e9821bfb1ff41860fa366fc90
Cristian Fernandez <cristian.fernandez.del.pozo@gmail.com>
2021-12-09 20:05:31 +0000

    add login screen structure

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,39 +1,47 @@
 import * as React from "react";
-import { StyleSheet } from "react-native";
+import { Button, StyleSheet, TextInput } from "react-native";
 
-import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
 import { RootTabScreenProps } from "../types";
 
 export default function LoginScreen({
   navigation,
-}: RootTabScreenProps<"TabOne">) {
+}: RootTabScreenProps<"Login">) {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Login Screen</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} placeholder="Usuario" />
+        <TextInput style={styles.input} placeholder="Contraseña" />
+        <Button title="Acceder" onPress={() => {}} />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    width: "100%",
+    height: "100%",
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
   },
   title: {
-    fontSize: 20,
+    fontSize: 40,
     fontWeight: "bold",
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
+  inputContainer: {
+    flex: 1,
+    width: "75%",
+    maxHeight: "25%",
+    alignItems: "stretch",
+    justifyContent: "flex-start",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
